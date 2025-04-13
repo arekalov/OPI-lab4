59a86d632f04b3e8ce13e75f733ec1c967f23572
arekalov <artyom.rekalov@gmail.com>
2025-04-13 17:02:20 +0300

    [refactor] remove redundant tasks and other trash

--- buildSrc/src/main/kotlin/com/arekalov/jsfgraph/BuildParams.kt
+++
@@ -1,7 +0,0 @@
-package com.arekalov.jsfgraph
-
-class BuildParams {
-    companion object{
-        const val TASKS_GROUP_NAME = "tasksGroupName"
-    }
-}
--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ echo "Deploying to Helios"
 ## Remove existing deployment
 ssh -p 2222 s409449@se.ifmo.ru "rm -rf /home/studs/s409449/Web/lab3/wildfly-preview-26.1.3.Final/standalone/deployments/interactive-graph-ui-1.0-SNAPSHOT*"
 # add new deployment
-scp -P 2222  /Users/arekalov/Itmo/4/OPI/lab3/opi-lab3/build/libs/interactive-graph-ui-1.0-SNAPSHOT.war s409449@se.ifmo.ru:/home/studs/s409449/Web/lab3/wildfly-preview-26.1.3.Final/standalone/deployments
+scp -P 2222  /Users/arekalov/Itmo/4/OPI/lab4/build/libs/interactive-graph-ui-1.0-SNAPSHOT.war s409449@se.ifmo.ru:/home/studs/s409449/Web/lab3/wildfly-preview-26.1.3.Final/standalone/deployments
